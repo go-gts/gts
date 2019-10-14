2015c79c862297386ee4b520cfef48546b14562f
Kotone Itaya <kotone@sfc.keio.ac.jp>
2019-10-14 18:46:17 +0900

    Add gcskew utility

---
+++ cmd/gcskew/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ktnyt/gt1"
+	"github.com/ktnyt/gt1/flags"
+)
+
+func main() {
+	prog, args := os.Args[0], os.Args[1:]
+	command := flags.NewCommand(prog, "Calculate the GC skew (or related metrics) of the given sequence.")
+	infile := command.Infile("input sequence file")
+	outfile := command.Outfile("output text file")
+	cumulative := command.Switch('c', "cumulative", "calculate cumulative skew")
+	metric := command.Choice('m', "metric", "bases to calculate skewness for", "gc", "at", "purine", "keto")
+	window := command.Int('w', "window", 10000, "window size")
+	slide := command.Int('s', "slide", *window, "slide size")
+
+	sets := [][]string{
+		[]string{"g", "c"},
+		[]string{"a", "t"},
+		[]string{"ag", "tc"},
+		[]string{"tg", "ac"},
+	}
+
+	command.Run(args, func() error {
+		seq, err := gt1.ReadSeq(infile)
+		if err != nil {
+			return err
+		}
+
+		set := sets[*metric]
+
+		fragments := gt1.Fragment(seq, *window, *slide)
+		pos := 0
+		skew := 0.
+
+		for _, fragment := range fragments {
+			if !*cumulative {
+				skew = 0.
+			}
+			skew += gt1.Skew(fragment, set[0], set[1])
+			fmt.Fprintf(outfile, "%d\t%f\n", pos, skew)
+			pos += *slide
+		}
+
+		return nil
+	})
+}
