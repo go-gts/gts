3e1950a06afc0e3042299a61805716166b00d940
Kotone Itaya <kotone@sfc.keio.ac.jp>
2020-10-02 22:35:53 +0900

    Add conda package files

---
+++ conda/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -ex
+
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/share/man/man1"
+mkdir -p "$PREFIX/share/man/man7"
+
+cp "$SRC_DIR/gts" "$PREFIX/bin"
+cp "$SRC_DIR/togo" "$PREFIX/bin"
+
+chmod +x "$PREFIX/bin/gts"
+chmod +x "$PREFIX/bin/togo"
+
+for FILE in "$SRC_DIR"/manpage/*.1; do
+    cp "$FILE" "$PREFIX/share/man/man1"
+done
+
+for FILE in "$SRC_DIR"/manpage/*.7; do
+    cp "$FILE" "$PREFIX/share/man/man7"
+done
