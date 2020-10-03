5c6289d1e910cc50f5ea25d059e3ddfa3fb6ddda
Kotone Itaya <kotone@sfc.keio.ac.jp>
2020-10-03 21:35:50 +0900

    Add completion script install configuration

--- conda/build.sh
+++ conda/build.sh
@@ -5,6 +5,8 @@ set -ex
 mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/share/man/man1"
 mkdir -p "$PREFIX/share/man/man7"
+mkdir -p "$PREFIX/share/bash-completion/completions"
+mkdir -p "$PREFIX/share/zsh/site-functions"
 
 cp "$SRC_DIR/gts" "$PREFIX/bin"
 cp "$SRC_DIR/togo" "$PREFIX/bin"
@@ -19,3 +21,6 @@ done
 for FILE in "$SRC_DIR"/man/*.7; do
     cp "$FILE" "$PREFIX/share/man/man7"
 done
+
+cp "$SRC_DIR/completions/gts-completion.bash" "$PREFIX/share/bash-completion/completions"
+cp "$SRC_DIR/completions/gts-completion.bash" "$PREFIX/share/zsh/site-functions"
