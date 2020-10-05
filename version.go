9521e6afb08eddf6db82180101f5d02880aad829
Kotone Itaya <kotone@sfc.keio.ac.jp>
2020-10-05 23:24:55 +0900

    Bump version v0.23.0

--- version.go
+++ version.go
@@ -5,6 +5,6 @@ import "github.com/go-gts/gts/internal/flags"
 // Version represents the GTS software version.
 var Version = flags.Version{
 	Major: 0,
-	Minor: 22,
-	Patch: 4,
+	Minor: 23,
+	Patch: 0,
 }
