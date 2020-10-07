478c38161ad5c577aa48aafab40d411f3b8f98ea
Kotone Itaya <kotone@sfc.keio.ac.jp>
2020-10-07 16:10:05 +0900

    Add pick and sort

---
+++ cmd/gts/pick.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/go-gts/gts"
+	"github.com/go-gts/gts/cmd"
+	"github.com/go-gts/gts/internal/flags"
+	"github.com/go-gts/gts/seqio"
+)
+
+func init() {
+	flags.Register("pick", "pick sequence(s) from multiple sequences", pickFunc)
+}
+
+type picker func(i int) bool
+
+func pickAll(pickers ...picker) picker {
+	return func(i int) bool {
+		for _, pick := range pickers {
+			if !pick(i) {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+func pickAny(pickers ...picker) picker {
+	return func(i int) bool {
+		for _, pick := range pickers {
+			if pick(i) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+func pickAfter(m int) picker {
+	return func(i int) bool {
+		return m <= i
+	}
+}
+
+func pickBefore(n int) picker {
+	return func(i int) bool {
+		return i <= n
+	}
+}
+
+func pickBetween(m, n int) picker {
+	return pickAll(pickAfter(m), pickBefore(n))
+}
+
+func pickOne(n int) picker {
+	return func(i int) bool {
+		return n == i
+	}
+}
+
+func mustAtoi(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func asPicker(list string) picker {
+	blocks := strings.Split(list, ",")
+	pickers := make([]picker, len(blocks))
+	for i, block := range blocks {
+		index := strings.IndexByte(block, '-')
+		switch index {
+		case -1:
+			n := mustAtoi(block)
+			pickers[i] = pickOne(n)
+		case 0:
+			n := mustAtoi(block[index+1:])
+			pickers[i] = pickBefore(n)
+		case len(block) - 1:
+			m := mustAtoi(block[:index])
+			pickers[i] = pickAfter(m)
+		default:
+			n := mustAtoi(block[index+1:])
+			m := mustAtoi(block[:index])
+			pickers[i] = pickBetween(m, n)
+		}
+	}
+	return pickAny(pickers...)
+}
+
+func pickFunc(ctx *flags.Context) error {
+	pos, opt := flags.Flags()
+
+	list := pos.String("list", "list of sequences to pick (identical to the list option in cut)")
+
+	var seqinPath *string
+	if cmd.IsTerminal(os.Stdin.Fd()) {
+		seqinPath = pos.String("seqin", "input sequence file (may be omitted if standard input is provided)")
+	}
+
+	seqoutPath := opt.String('o', "output", "-", "output sequence file (specifying `-` will force standard output)")
+	feature := opt.Switch('f', "feature", "pick features instead of sequences")
+	format := opt.String('F', "format", "", "output file format (defaults to same as input)")
+
+	if err := ctx.Parse(pos, opt); err != nil {
+		return err
+	}
+
+	pick := asPicker(*list)
+
+	seqinFile := os.Stdin
+	if seqinPath != nil && *seqinPath != "-" {
+		f, err := os.Open(*seqinPath)
+		if err != nil {
+			return ctx.Raise(fmt.Errorf("failed to open file %q: %v", *seqinPath, err))
+		}
+		seqinFile = f
+		defer seqinFile.Close()
+	}
+
+	seqoutFile := os.Stdout
+	if *seqoutPath != "-" {
+		f, err := os.Create(*seqoutPath)
+		if err != nil {
+			return ctx.Raise(fmt.Errorf("failed to create file %q: %v", *seqoutPath, err))
+		}
+		seqoutFile = f
+		defer seqoutFile.Close()
+	}
+
+	filetype := seqio.Detect(*seqoutPath)
+	if *format != "" {
+		filetype = seqio.ToFileType(*format)
+	}
+
+	w := bufio.NewWriter(seqoutFile)
+
+	scanner := seqio.NewAutoScanner(seqinFile)
+	i := 0
+	for scanner.Scan() {
+		seq := scanner.Value()
+		i++
+
+		if pick(i) || *feature {
+			if *feature {
+				ff := []gts.Feature{}
+				for j, f := range seq.Features() {
+					if pick(j) {
+						ff = append(ff, f)
+					}
+				}
+				seq = gts.WithFeatures(seq, ff)
+			}
+
+			formatter := seqio.NewFormatter(seq, filetype)
+
+			if _, err := formatter.WriteTo(w); err != nil {
+				return ctx.Raise(err)
+			}
+
+			if err := w.Flush(); err != nil {
+				return ctx.Raise(err)
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return ctx.Raise(fmt.Errorf("encountered error in scanner: %v", err))
+	}
+
+	return nil
+}
---
+++ cmd/gts/sort.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/go-gts/gts"
+	"github.com/go-gts/gts/cmd"
+	"github.com/go-gts/gts/internal/flags"
+	"github.com/go-gts/gts/seqio"
+)
+
+func init() {
+	flags.Register("sort", "sort the list of sequences", sortFunc)
+}
+
+type byLength []gts.Sequence
+
+func (ss byLength) Len() int {
+	return len(ss)
+}
+
+func (ss byLength) Less(i, j int) bool {
+	return gts.Len(ss[j]) < gts.Len(ss[i])
+}
+
+func (ss byLength) Swap(i, j int) {
+	ss[i], ss[j] = ss[j], ss[i]
+}
+
+func sortFunc(ctx *flags.Context) error {
+	pos, opt := flags.Flags()
+
+	var seqinPath *string
+	if cmd.IsTerminal(os.Stdin.Fd()) {
+		seqinPath = pos.String("seqin", "input sequence file (may be omitted if standard input is provided)")
+	}
+
+	seqoutPath := opt.String('o', "output", "-", "output sequence file (specifying `-` will force standard output)")
+	reverse := opt.Switch('r', "reverse", "reverse the sort order")
+	format := opt.String('F', "format", "", "output file format (defaults to same as input)")
+
+	if err := ctx.Parse(pos, opt); err != nil {
+		return err
+	}
+
+	seqinFile := os.Stdin
+	if seqinPath != nil && *seqinPath != "-" {
+		f, err := os.Open(*seqinPath)
+		if err != nil {
+			return ctx.Raise(fmt.Errorf("failed to open file %q: %v", *seqinPath, err))
+		}
+		seqinFile = f
+		defer seqinFile.Close()
+	}
+
+	seqoutFile := os.Stdout
+	if *seqoutPath != "-" {
+		f, err := os.Create(*seqoutPath)
+		if err != nil {
+			return ctx.Raise(fmt.Errorf("failed to create file %q: %v", *seqoutPath, err))
+		}
+		seqoutFile = f
+		defer seqoutFile.Close()
+	}
+
+	filetype := seqio.Detect(*seqoutPath)
+	if *format != "" {
+		filetype = seqio.ToFileType(*format)
+	}
+
+	w := bufio.NewWriter(seqoutFile)
+
+	seqs := []gts.Sequence{}
+
+	scanner := seqio.NewAutoScanner(seqinFile)
+	for scanner.Scan() {
+		seq := scanner.Value()
+		seqs = append(seqs, seq)
+	}
+
+	var iface sort.Interface
+	iface = byLength(seqs)
+	if *reverse {
+		iface = sort.Reverse(iface)
+	}
+	sort.Sort(iface)
+
+	for _, seq := range seqs {
+		formatter := seqio.NewFormatter(seq, filetype)
+		if _, err := formatter.WriteTo(w); err != nil {
+			return ctx.Raise(err)
+		}
+
+		if err := w.Flush(); err != nil {
+			return ctx.Raise(err)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return ctx.Raise(fmt.Errorf("encountered error in scanner: %v", err))
+	}
+
+	return nil
+}
