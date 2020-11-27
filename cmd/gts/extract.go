eb17ace2e6e9ce06ce21cb46b02b9710d718c574
Kotone Itaya <kotone@sfc.keio.ac.jp>
2020-11-27 17:00:54 +0900

    Fix extract logic

--- cmd/gts/extract.go
+++ cmd/gts/extract.go
@@ -20,10 +20,10 @@ func init() {
 func containsRegion(rr []gts.Region, r gts.Region) bool {
 	for i := range rr {
 		if reflect.DeepEqual(rr[i], r) {
-			return false
+			return true
 		}
 	}
-	return true
+	return false
 }
 
 func extractFunc(ctx *flags.Context) error {
@@ -94,20 +94,22 @@ func extractFunc(ctx *flags.Context) error {
 		rr := make([]gts.Region, 0)
 		for _, locate := range locators {
 			for _, r := range locate(seq) {
-				if r.Len() != gts.Len(seq) || !containsRegion(rr, r) {
+				if !containsRegion(rr, r) {
 					rr = append(rr, r)
 				}
 			}
 		}
 
 		for _, region := range rr {
-			out := region.Locate(seq)
-			formatter := seqio.NewFormatter(out, filetype)
-			if _, err := formatter.WriteTo(w); err != nil {
-				return ctx.Raise(err)
-			}
-			if err := w.Flush(); err != nil {
-				return ctx.Raise(err)
+			if len(rr) == 1 || region.Len() != gts.Len(seq) {
+				out := region.Locate(seq)
+				formatter := seqio.NewFormatter(out, filetype)
+				if _, err := formatter.WriteTo(w); err != nil {
+					return ctx.Raise(err)
+				}
+				if err := w.Flush(); err != nil {
+					return ctx.Raise(err)
+				}
 			}
 		}
 	}
